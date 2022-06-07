548df35c8506aced3c333bf235c7d980b855bc08
Pai Peng <sipaipv6@gmail.com>
2022-06-07 16:29:59 +0800

    reformat

--- pdfutil.cpp
+++ pdfutil.cpp
@@ -70,131 +70,137 @@ PDFUtil::PDFUtil()
 }
 
 int PDFUtil::generatePdf(QString data) {
-    const char *page_title = "Text Demo";
+    const char *page_title = "CI Label";
 
-        HPDF_Doc  pdf;
-        HPDF_Font font;
-        HPDF_Page page;
-        char fname[256];
+    HPDF_Doc  pdf;
+    HPDF_Font font;
+    HPDF_Page page;
+    char fname[256];
 
-        const char* samp_text = "abcdef";
-        float tw;
-        float fsize;
-        int i;
+    float tw;
+    float fsize;
+    int i;
+    int len;
+
+    float angle1;
+    float angle2;
+    float rad1;
+    float rad2;
+
+    float ypos;
+
+    strcpy (fname, "qt_text_pdf_test");
+    strcat (fname, ".pdf");
+
+    pdf = HPDF_New (error_handler, NULL);
+    if (!pdf) {
+        printf ("error: cannot create PdfDoc object\n");
+        return 1;
+    }
+
+    if (setjmp(env)) {
+        HPDF_Free (pdf);
+        return 1;
+    }
+
+    /* set compression mode */
+    HPDF_SetCompressionMode (pdf, HPDF_COMP_ALL);
+
+#if 0
+    HPDF_UseCNTFonts(pdf);
+    HPDF_UseCNTEncodings(pdf);
+#endif
+
+#if 1
+    /* create default-font */
+    /* create default-font */
+    font = HPDF_GetFont (pdf, "Helvetica", NULL);
+
+#else
+    HPDF_UseCNSFonts(pdf);
+    HPDF_UseCNSEncodings(pdf);
+
+    HPDF_UseUTFEncodings(pdf);
+    HPDF_SetCurrentEncoder(pdf, "UTF-8");
+    const char* font_name = HPDF_LoadTTFontFromFile(pdf, "C:\\Users\\paipeng\\git\\libharu\\demo\\ttfont\\FZQTJW.TTF", HPDF_TRUE);
+    font = HPDF_GetFont(pdf, font_name, "UTF-8");
+#endif
+
+    /* add a new page object. */
+    page = HPDF_AddPage (pdf);
+    //HPDF_Page_SetSize (page, HPDF_PAGE_SIZE_A5, HPDF_PAGE_LANDSCAPE);
+    // 50x30mm 72ppi
+    // 1inch = 25.4 mm
+    float mm_inch = 0.0393701;
+    HPDF_Page_SetWidth(page, 50 * mm_inch * 72);
+    HPDF_Page_SetHeight(page, 30 * mm_inch * 72);
+
+    /* draw grid to the page */
+    //print_grid  (pdf, page);
+
+    /* print the lines of the page.
+    HPDF_Page_SetLineWidth (page, 1);
+    HPDF_Page_Rectangle (page, 50, 50, HPDF_Page_GetWidth(page) - 100,
+                HPDF_Page_GetHeight (page) - 110);
+    HPDF_Page_Stroke (page);
+    */
+
+    /* print the title of the page (with positioning center). */
+    HPDF_Page_SetFontAndSize (page, font, 12);
+    tw = HPDF_Page_TextWidth (page, page_title);
+    HPDF_Page_BeginText (page);
+    HPDF_Page_TextOut (page, (HPDF_Page_GetWidth(page) - tw) / 2,
+                HPDF_Page_GetHeight (page) - 20, page_title);
+    HPDF_Page_EndText (page);
+
+    HPDF_Page_BeginText (page);
+    HPDF_Page_MoveTextPos (page, 60, HPDF_Page_GetHeight(page) - 60);
+
+    /*
+     * font size
+     */
+    fsize = 8;
+    while (fsize < 9) {
+        char buf[50];
         int len;
 
-        float angle1;
-        float angle2;
-        float rad1;
-        float rad2;
+        /* set style and size of font. */
+        HPDF_Page_SetFontAndSize(page, font, fsize);
 
-        float ypos;
+        /* set the position of the text. */
+        HPDF_Page_MoveTextPos (page, 0, -5 - fsize);
 
-        strcpy (fname, "qt_text_pdf_test");
-        strcat (fname, ".pdf");
+        /* measure the number of characters which included in the page. */
+        const char *samp_text = data.toStdString().data();
+        strcpy(buf, samp_text);
+        len = HPDF_Page_MeasureText (page, samp_text,
+                        HPDF_Page_GetWidth(page) - 120, HPDF_FALSE, NULL);
 
-        pdf = HPDF_New (error_handler, NULL);
-        if (!pdf) {
-            printf ("error: cannot create PdfDoc object\n");
-            return 1;
-        }
+        /* truncate the text. */
+        buf[len] = 0x00;
 
-        if (setjmp(env)) {
-            HPDF_Free (pdf);
-            return 1;
-        }
+        HPDF_Page_ShowText (page, samp_text);
+#if 0
+        /* print the description. */
+        HPDF_Page_MoveTextPos (page, 0, -10);
+        HPDF_Page_SetFontAndSize(page, font, 8);
+        #ifdef __WIN32__
+        _snprintf(buf, 50, "Fontsize=%.0f", fsize);
+        #else
+        snprintf(buf, 50, "Fontsize=%.0f", fsize);
+        #endif
+        HPDF_Page_ShowText (page, buf);
 
-        /* set compression mode */
-        HPDF_SetCompressionMode (pdf, HPDF_COMP_ALL);
-
-    #if 0
-        HPDF_UseCNTFonts(pdf);
-        HPDF_UseCNTEncodings(pdf);
-    #endif
-
-    #if 1
-        /* create default-font */
-        /* create default-font */
-        font = HPDF_GetFont (pdf, "Helvetica", NULL);
-
-    #else
-        HPDF_UseCNSFonts(pdf);
-        HPDF_UseCNSEncodings(pdf);
-
-        HPDF_UseUTFEncodings(pdf);
-        HPDF_SetCurrentEncoder(pdf, "UTF-8");
-        const char* font_name = HPDF_LoadTTFontFromFile(pdf, "C:\\Users\\paipeng\\git\\libharu\\demo\\ttfont\\FZQTJW.TTF", HPDF_TRUE);
-        font = HPDF_GetFont(pdf, font_name, "UTF-8");
-    #endif
-
-        /* add a new page object. */
-        page = HPDF_AddPage (pdf);
-        HPDF_Page_SetSize (page, HPDF_PAGE_SIZE_A5, HPDF_PAGE_LANDSCAPE);
-
-        /* draw grid to the page */
-        //print_grid  (pdf, page);
-
-        /* print the lines of the page.
-        HPDF_Page_SetLineWidth (page, 1);
-        HPDF_Page_Rectangle (page, 50, 50, HPDF_Page_GetWidth(page) - 100,
-                    HPDF_Page_GetHeight (page) - 110);
-        HPDF_Page_Stroke (page);
-        */
-
-        /* print the title of the page (with positioning center). */
-        HPDF_Page_SetFontAndSize (page, font, 24);
-        tw = HPDF_Page_TextWidth (page, page_title);
-        HPDF_Page_BeginText (page);
-        HPDF_Page_TextOut (page, (HPDF_Page_GetWidth(page) - tw) / 2,
-                    HPDF_Page_GetHeight (page) - 50, page_title);
-        HPDF_Page_EndText (page);
-
-        HPDF_Page_BeginText (page);
-        HPDF_Page_MoveTextPos (page, 60, HPDF_Page_GetHeight(page) - 60);
-
-        /*
-         * font size
-         */
-        fsize = 8;
-        while (fsize < 60) {
-            char buf[50];
-            int len;
-
-            /* set style and size of font. */
-            HPDF_Page_SetFontAndSize(page, font, fsize);
-
-            /* set the position of the text. */
-            HPDF_Page_MoveTextPos (page, 0, -5 - fsize);
-
-            /* measure the number of characters which included in the page. */
-            strcpy(buf, samp_text);
-            len = HPDF_Page_MeasureText (page, samp_text,
-                            HPDF_Page_GetWidth(page) - 120, HPDF_FALSE, NULL);
-
-            /* truncate the text. */
-            buf[len] = 0x00;
-
-            HPDF_Page_ShowText (page, buf);
-
-            /* print the description. */
-            HPDF_Page_MoveTextPos (page, 0, -10);
-            HPDF_Page_SetFontAndSize(page, font, 8);
-            #ifdef __WIN32__
-            _snprintf(buf, 50, "Fontsize=%.0f", fsize);
-            #else
-            snprintf(buf, 50, "Fontsize=%.0f", fsize);
-            #endif
-            HPDF_Page_ShowText (page, buf);
-
-            fsize *= 1.5;
-        }
+#endif
+        fsize *= 1.5;
+    }
 
 
-        /* save the document to a file */
-        HPDF_SaveToFile (pdf, fname);
+    /* save the document to a file */
+    HPDF_SaveToFile (pdf, fname);
 
-        /* clean up */
-        HPDF_Free (pdf);
+    /* clean up */
+    HPDF_Free (pdf);
 
-        return 0;
+    return 0;
 }
