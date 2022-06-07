ece13f2bbdf19d027c0b505de3b041bb73abbfa3
Pai Peng <sipaipv6@gmail.com>
2022-06-07 18:17:35 +0800

    update language

--- qt-label-print_zh_CN.ts
+++ qt-label-print_zh_CN.ts
@@ -1,3 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_CN"></TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished">QT-标签设计打印</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="27"/>
+        <source>generate_pdf</source>
+        <translation type="unfinished">生成PDF</translation>
+    </message>
+</context>
+</TS>
